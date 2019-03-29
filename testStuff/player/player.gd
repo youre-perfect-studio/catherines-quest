6c6650e0179ddd0a9fd5d9e21adc5e87cd5efdaf
Cloud Lusich <alusich@gmail.com>
2019-03-29 05:33:32 -0600

    Add Magnet (not functioning)
    Had to fix bridge again due to issue with tileset, had severe crash issues attempting this, added Magnet item and some other nodes and small collision to it.  May have to change items from static  body to kinematic in order to make magnet work with everything and the bat later.

---
+++ testStuff/player/player.gd
@@ -0,0 +1,23 @@
+extends KinematicBody2D
+
+var movedir = Vector2.ZERO
+var speed = 40
+
+#warning-ignore:unused_argument
+func _process(delta):
+	controlLoop()
+	movementLoop()
+	
+func controlLoop():
+	var left = Input.is_action_pressed("ui_left")
+	var right = Input.is_action_pressed("ui_right")
+	var up = Input.is_action_pressed("ui_up")
+	var down = Input.is_action_pressed("ui_down")
+	
+	movedir.x = -int(left) + int(right)
+	movedir.y = -int(up) + int(down)
+	
+func movementLoop():
+	var motion
+	motion = movedir.normalized() * speed
+	move_and_slide(motion, Vector2.ZERO)
