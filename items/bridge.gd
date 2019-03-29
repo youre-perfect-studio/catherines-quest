224c187cff13824b3a5a7495f10962bdc7bdd8b5
Cloud Lusich <alusich@gmail.com>
2019-03-28 23:51:13 -0600

    Bridge added
    Working bridge added, tempory bridge graphic to be replaced when avalible.

---
+++ items/bridge.gd
@@ -0,0 +1,13 @@
+extends Area2D
+
+func _ready():
+	connect("area_entered", self, "onAreaEntered")
+	connect("area_exited", self, "onAreaExited")
+	
+func onAreaEntered(area):
+	if area.name == "player":
+		area.get_parent().set_collision_mask_bit(0, 0)
+		
+func onAreaExited(area):
+	if area.name == "player":
+		area.get_parent().set_collision_mask_bit(0,1)
