b42375a1fd13bb43c4de911716696c6adc4f8491
Damain Joseph <damainj@kavierservices.com>
2019-04-12 08:03:42 -0400

    Added Animation player, placeholder sprite and walking animation

--- testStuff/entity.gd
+++ testStuff/entity.gd
@@ -3,6 +3,8 @@ extends KinematicBody2D
 export(String) var type = "enemy"
 export(String) var subType
 
+var spritedir = "left"
+
 var movedir = Vector2.ZERO
 
 var chasing = false
@@ -26,3 +28,20 @@ func pickDirection():
 			return Vector2(x, 0)
 		2:
 			return Vector2(x, y)
+			
+func spritedirLoop():
+	match movedir:
+		Vector2(-1,0):
+			spritedir = "left"
+		Vector2(1,0):
+			spritedir = "right"
+		Vector2(0,-1):
+			spritedir = "up"
+		Vector2(0,1):
+			spritedir = "down"
+
+func anim_switch(action):
+	$Anim.play(action+spritedir)   
+	
+func damageLoop():
+	pass
