00a96d1b8c9c803f7c5a8b66fc31dabd472e354c
Catherine and Katrina <youreperfectstudio@gmail.com>
2019-04-14 23:14:37 -0500

    Start on character resources

---
+++ src/characters/Character.gd
@@ -0,0 +1,10 @@
+extends Resource
+class_name Character
+
+export(String) var character_name
+export(bool) var playable_character
+export(PackedScene) var character_scene
+export(String) var pronouns
+export(Texture) var symbol
+export(String,MULTILINE) var backstory
+export(Dictionary) var expressions
