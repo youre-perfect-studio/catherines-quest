6b6a0836f822e2a426881489981cd7c25e75b73d
Catherine and Katrina <youreperfectstudio@gmail.com>
2019-04-23 16:13:56 -0500

    Try using 3.1.1-rc1

--- run_tests.sh
+++ run_tests.sh
@@ -1,7 +1,7 @@
-GODOT_VERSION=3.1
-GODOT_BIN=Godot_v${GODOT_VERSION}-stable_linux_headless.64
+GODOT_VERSION=3.1.1
+GODOT_BIN=Godot_v${GODOT_VERSION}-rc1_linux_headless.64
 GODOT_ZIP=${GODOT_BIN}.zip
-GODOT_URL=https://downloads.tuxfamily.org/godotengine/${GODOT_VERSION}/${GODOT_ZIP}
+GODOT_URL=https://downloads.tuxfamily.org/godotengine/${GODOT_VERSION}/rc1/${GODOT_ZIP}
 
 curl ${GODOT_URL} --output ${GODOT_ZIP}
 unzip ${GODOT_ZIP}
