bc3327dae99e248b6abe9da21bd07fb03aebd8b4
Catherine and Katrina <youreperfectstudio@gmail.com>
2019-04-22 22:54:21 -0500

    Allow conversing with Catherine

--- src/story/engine/DialogController.gd
+++ src/story/engine/DialogController.gd
@@ -3,12 +3,23 @@ extends CanvasLayer
 func _ready():
 	pass
 
+func set_left_portrait( portrait:Texture ):
+	$Container/LeftPortrait.texture = portrait
+
+func set_right_portrait( portrait:Texture ):
+	$Container/RightPortrait.texture = portrait
+
+func set_speaker( name:String ):
+	$Container/Speaker.text = name
+
+func set_text( text:String ):
+	$Container/DialogText.text = text
 
 func _on_close_button_pressed():
 	hide_workaround()
 
 # see https://github.com/godotengine/godot/issues/16532
 func show_workaround():
-	self.scale = Vector2.ONE
+	$Container.show()
 func hide_workaround():
-	self.scale = Vector2.ZERO
+	$Container.hide()
