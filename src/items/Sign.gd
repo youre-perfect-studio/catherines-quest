9cee80f0ec495886f9664cef1a078c45caf2e673
Damain Joseph <damainj@kavierservices.com>
2019-04-24 14:04:17 -0400

    adding sign popup menu

--- src/items/Sign.gd
+++ src/items/Sign.gd
@@ -1,8 +1,16 @@
 extends Node2D
 
+
 func _ready():
 	$Area2D.connect("area_entered", self, "open_menu")
+	$Area2D.connect("area_exited", self, "close_menu")
+	
 
 func open_menu(area):
+	if area.get_parent().name == "player":
+		$SaveMenu.popup()
+		print("opening menu")
 	
-	print("opening menu")
+func close_menu(area):
+	if area.get_parent().name == "player":
+		$SaveMenu.close()
