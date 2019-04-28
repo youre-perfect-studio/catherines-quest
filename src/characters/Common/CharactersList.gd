4e9244b349da7cfc6e1650ee009796251967dae8
Catherine and Katrina <youreperfectstudio@gmail.com>
2019-04-27 19:37:02 -0500

    Fix portrait loading in exported builds
    Workaroud for this Godot Engine bug :https://github.com/godotengine/godot/issues/25672

--- src/characters/Common/CharactersList.gd
+++ src/characters/Common/CharactersList.gd
@@ -3,28 +3,16 @@ Copyright(C) 2019 You're Perfect LLC, All Rights Reserved
 Licensed under the MIT license, see LICENSE file in the
 project root folder for more information.
 
-Inspired by https://github.com/GDquest/godot-open-rpg/blob/master/godot/dialogue/DialogueDatabase.gd
+
 """
 extends Object
 class_name CharactersList
 
-const CHARACTERS_DIRECTORY = "res://characters"
-var characters : Dictionary
-
-func _init():
-	var dir = Directory.new()
-	assert dir.dir_exists(CHARACTERS_DIRECTORY)
-	if dir.open(CHARACTERS_DIRECTORY) != OK:
-		print("Could not open %s" % CHARACTERS_DIRECTORY)
-	dir.list_dir_begin()
-	var file_name : String
-	while true: 	#Godot doesn't have do-while
-		file_name = dir.get_next()
-		if file_name == "":
-			break
-		elif not file_name.ends_with(".tres"):
-			continue
-		characters[file_name.get_basename()] = load(CHARACTERS_DIRECTORY.plus_file(file_name))
+var characters : Dictionary = {
+	 "Robin"		:	preload("res://characters/Robin.tres"),
+	"Catherine"		:	preload("res://characters/Catherine.tres"),
+	"ComingSoon"	:	preload("res://characters/ComingSoon.tres"),
+	}
 
 func get_portrait(character_name:String, expression:String = "default") -> Texture:
 	assert(character_name in characters)
