237716d815bf8b882d1774661451e9033cb544ee
Catherine and Katrina <youreperfectstudio@gmail.com>
2019-04-28 20:41:03 -0500

    Auto-select robin if no character selected (fixes F6 debugging)

--- src/screens/Episode1.gd
+++ src/screens/Episode1.gd
@@ -5,7 +5,11 @@ func _ready():
 	call_deferred("spawn_players")
 
 func spawn_players():
-	assert(Globals.Players.size()>0)
+	
+	#This should only happe when running single-scene debugging
+	if Globals.Players.size() == 0:
+		Globals.Players.append(CharactersList.new().characters["Robin"].character_scene.instance())
+		
 	#TODO when character selection works
 	#for player in Globals.Players:
 	#	$"/root/Episode1".add_child(player)
