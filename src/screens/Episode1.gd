aaf6d7865e06277df481fe8cc8f15186502e2174
Damain Joseph <damainj@kavierservices.com>
2019-04-28 21:37:22 -0400

    adding save functionality

--- src/screens/Episode1.gd
+++ src/screens/Episode1.gd
@@ -5,8 +5,15 @@ func _ready():
 	call_deferred("spawn_players")
 
 func spawn_players():
-	assert(Globals.Players.size()>0)
+	#assert(Globals.Players.size()>0)
 	#TODO when character selection works
 	#for player in Globals.Players:
 	#	$"/root/Episode1".add_child(player)
 	$player.show_opening_dialog()
+	
+func save():
+	var save_dict = {
+		"filename": get_filename(),
+		"parent": get_parent(),
+	}
+	return save_dict
