2c94fdc759f9662bc73a4965d30eb57a5d669b83
Damain Joseph <damainj@kavierservices.com>
2019-04-30 09:55:25 -0400

    Fixed issue #6 click counts as key

--- src/title.gd
+++ src/title.gd
@@ -2,8 +2,14 @@ extends Node2D
 
 func _ready():
 	$Anim.play("animateText")
+	$Button.connect("pressed", self, "start_game")
 	
 func _input(event):
-	if event is InputEventKey and event.pressed:
+	if event is InputEventKey and event.pressed :
 		#print("any key was pressed")
-		get_tree().change_scene("res://menus/gameSelection.tscn")
+		start_game()
+		
+
+
+func start_game():
+	get_tree().change_scene("res://menus/gameSelection.tscn")
